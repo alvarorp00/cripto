2beaec935dfbd6d12cde43402bee7ca09daabb60
alvarorp00 <alvarorp00@sigsuspend.net>
2021-12-13 19:40:13 +0100

    safe commit before testing exponentiation

--- p3/rsa/code/mains/power_main.c
+++ p3/rsa/code/mains/power_main.c
@@ -101,7 +101,7 @@ static void runExample(power_t *data, power_params_t *params, FILE *o_file)
 #include "sodium.h"
 
 #define PRINT_TEST_HEADER(f) \
-  fprintf(f, "BASE\tEXPONENT(avg)\tMODULUS(avg)\tSELF (ms)\tGMP (ms)\tDIFFERENCE\n");
+  fprintf(f, "BASE\tEXPONENT\tMODULUS\tSELF(ms)\tGMP(ms)\tDIFFERENCE\n");
 
 size_t currentTimeInMiliseconds()
 {
@@ -113,8 +113,8 @@ size_t currentTimeInMiliseconds()
 
 /** 
  * base starting with 96 bits until ~4000b, steps of 32b
- * exponent starting with 8 bits least until 16 bits chosen randomly
- * modulus starting in [3-33] until 1000 with steps of [3-33] in a cyclic way
+ * exponent chosen randomly between 256 and 65565
+ * modulus chosen randomly between 15 and 45
  */
  
 static void runTest(FILE *o_file)
@@ -122,18 +122,14 @@ static void runTest(FILE *o_file)
   #define TESTS 10000
   
   #define BASE_STEP 32
-  // #define MODL_STEP 3
-
   #define BASE_START 96
-  #define MODL_START 2
-
   #define BASE_TOP 4000
-  #define MODL_TOP 1000
+
+  #define MOD_BOT_BOUND 15
+  #define MOD_TOP_BOUND 45
   
   mpz_t base, exponent, module;
   mpz_t sres, gres; // self function && gmp function results
-
-  mpf_t mod_sum, exp_sum, _ftmp;
   
   power_t *data;
   mpz_power_error_t err;
@@ -146,8 +142,6 @@ static void runTest(FILE *o_file)
   size_t self_elapsed_ms;
   size_t gmp_elapsed_ms;
 
-  size_t iterations;
-
   // setup
   data = power_init();
   assert(data != NULL);
@@ -157,66 +151,44 @@ static void runTest(FILE *o_file)
   mpz_inits(base, exponent, module, NULL);
   mpz_inits(sres, gres, NULL);
 
-  mpf_inits(mod_sum, exp_sum, _ftmp, NULL);
-
-  mpz_set_ui(base, (unsigned long)(randombytes_random())); // 32 random bytes
+  mpz_set_ui(base, (unsigned long)(randombytes_random())); // 32 random bits
   mpz_mul_2exp(base, base, 32);
 
-  mpz_set_ui(base, (unsigned long)(randombytes_random())); // 32 random bytes
+  mpz_set_ui(base, (unsigned long)(randombytes_random())); // 32 random bits
   mpz_mul_2exp(base, base, 32);
 
-  mpz_set_ui(base, (unsigned long)(randombytes_random())); // 32 random bytes
+  mpz_set_ui(base, (unsigned long)(randombytes_random())); // 32 random bits
   mpz_mul_2exp(base, base, 32);
 
   // now we have 96 bits number in mpz
   
-  // gmp_printf("--> %s\n", mpz_get_str(NULL, 10, base));
-
   for(_bits = BASE_START; _bits < BASE_TOP; _bits += BASE_STEP)
   {
-    mpz_set_ui(module, (unsigned long)(randombytes_uniform(30) + 3));
+    mpz_set_ui(module, (unsigned long)(randombytes_uniform(MOD_BOT_BOUND) + (MOD_TOP_BOUND - MOD_BOT_BOUND))); // random module
+    mpz_set_ui(exponent, (unsigned long)(randombytes_uniform(UINT16_MAX) + UCHAR_MAX)); // random exponent
 
-    mpf_set_ui(mod_sum, 0L);
-    mpf_set_ui(exp_sum, 0L);
+    power_setup(data, mpz_get_str(NULL, 10, base), mpz_get_str(NULL, 10, exponent), mpz_get_str(NULL, 10, module));
+    starting_t = currentTimeInMiliseconds();
+    err = power_compute(data, sres);
+    ending_t = currentTimeInMiliseconds();
+    self_elapsed_ms = starting_t - ending_t;
 
-    iterations = 0;
-    while(mpz_cmp_ui(module, MODL_TOP) < 0)
-    {
-      mpz_set_ui(exponent, (unsigned long)(randombytes_uniform(UINT16_MAX) + UCHAR_MAX)); // random exponent
+    assert(err == 0); // assert is fine, do not take time because of time performance
 
-      power_setup(data, mpz_get_str(NULL, 10, base), mpz_get_str(NULL, 10, exponent), mpz_get_str(NULL, 10, module));
-      starting_t = currentTimeInMiliseconds();
-      err = power_compute(data, sres);
-      ending_t = currentTimeInMiliseconds();
-      self_elapsed_ms = starting_t - ending_t;
+    starting_t = currentTimeInMiliseconds();
+    mpz_powm(gres, base, exponent, module);
+    ending_t = currentTimeInMiliseconds();
+    gmp_elapsed_ms = starting_t - ending_t;
 
-      assert(err); // do not take time because of time performance
+    assert(mpz_cmp(sres, gres) == 0); // assert calcs are ok
 
-      starting_t = currentTimeInMiliseconds();
-      mpz_powm(gres, base, exponent, module);
-      ending_t = currentTimeInMiliseconds();
-      gmp_elapsed_ms = starting_t - ending_t;
-
-      assert(mpz_cmp(sres, gres) == 0);
-      
-      mpf_set_z(_ftmp, module);
-      mpf_add(mod_sum, mod_sum, _ftmp); // add module value
-      mpf_set_z(_ftmp, exponent);
-      mpf_add(exp_sum, exp_sum, _ftmp); // add exponent value
-      
-      mpz_add_ui(module, module, (unsigned long)(randombytes_uniform(30) + 3)); // alterate module
-      iterations++;
-    }
-    mpf_div_ui(mod_sum, mod_sum, iterations); // average value of module
-    mpf_div_ui(exp_sum, exp_sum, iterations); // average value of exponent
     // print here
-    gmp_fprintf(o_file, "%Zd\t%.3Ff\t%.3Ff\t%ld\t%ld\t%ld\n",
-      base, exp_sum, mod_sum, self_elapsed_ms, gmp_elapsed_ms, abs(self_elapsed_ms - gmp_elapsed_ms));
+    gmp_fprintf(o_file, "%Zd\t%Zd\t%Zd\t%ld\t%ld\t%ld\n",
+      base, exponent, module, self_elapsed_ms, gmp_elapsed_ms, abs(self_elapsed_ms - gmp_elapsed_ms));
     
-    mpz_mul_2exp(base, base, BASE_STEP);
-    mpz_add_ui(base, base, (unsigned long)(randombytes_random())); // 32 random bytes
+    mpz_mul_2exp(base, base, BASE_STEP); // shift left 32 bits
+    mpz_add_ui(base, base, (unsigned long)(randombytes_random())); // 32 random bits
   } 
   mpz_clears(base, exponent, module, NULL);
-  mpz_clears(sres, gres, NULL);   
-  mpf_clears(mod_sum, exp_sum, _ftmp, NULL);
+  mpz_clears(sres, gres, NULL);
 }
