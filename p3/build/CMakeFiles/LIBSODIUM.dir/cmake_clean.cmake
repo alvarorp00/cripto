8e1fe2a8737ed39d5a93cfcdb6ec4878fc1f4cee
alvarorp00 <alvarorp00@sigsuspend.net>
2021-12-13 12:36:15 +0100

    stable version of integer exponentiation
    modulus is not performed yet

---
+++ p3/build/CMakeFiles/LIBSODIUM.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "../external/libsodium/src/LIBSODIUM-stamp/LIBSODIUM-build"
+  "../external/libsodium/src/LIBSODIUM-stamp/LIBSODIUM-configure"
+  "../external/libsodium/src/LIBSODIUM-stamp/LIBSODIUM-download"
+  "../external/libsodium/src/LIBSODIUM-stamp/LIBSODIUM-install"
+  "../external/libsodium/src/LIBSODIUM-stamp/LIBSODIUM-mkdir"
+  "../external/libsodium/src/LIBSODIUM-stamp/LIBSODIUM-patch"
+  "../external/libsodium/src/LIBSODIUM-stamp/LIBSODIUM-update"
+  "CMakeFiles/LIBSODIUM"
+  "CMakeFiles/LIBSODIUM-complete"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/LIBSODIUM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ p3/build/bin/CMakeFiles/potenciacion.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "../executables/potenciacion"
+  "../executables/potenciacion.pdb"
+  "CMakeFiles/potenciacion.dir/code/mains/power_main.c.o"
+  "CMakeFiles/potenciacion.dir/code/src/logger.c.o"
+  "CMakeFiles/potenciacion.dir/code/src/params.c.o"
+  "CMakeFiles/potenciacion.dir/code/src/power.c.o"
+  "CMakeFiles/potenciacion.dir/code/src/util.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/potenciacion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
